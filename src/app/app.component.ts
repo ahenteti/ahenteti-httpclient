192388b4f3420df55970f2243ac4371afee0e8d7
ahenteti <ahmad.henteti@gmail.com>
2020-08-01 20:06:11 +0200

    add jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    jQuery('nav').resizable({
+      handles: 'e',
+    });
+    jQuery('header').resizable({
+      handles: 's',
+    });
+  }
+}
