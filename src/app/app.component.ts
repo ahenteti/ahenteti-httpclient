1ae9c61756ef4d2fed4a8e80f665dd56f13b824f
ahenteti <ahmad.henteti@gmail.com>
2020-08-02 00:05:34 +0200

    improve resizable-div component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,4 @@ declare var jQuery: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    jQuery('nav').resizable({
-      handles: 'e',
-    });
-    jQuery('header').resizable({
-      handles: 's',
-    });
-  }
-}
+export class AppComponent {}
