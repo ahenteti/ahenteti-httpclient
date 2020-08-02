e564d5a345bc9c073d8b89ec439faab48edad3ed
ahenteti <ahmad.henteti@gmail.com>
2020-08-02 22:12:19 +0200

    add electron scripts

--- bin/update-base-tag-for-electron-app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var path = require("path");
-var fs = require("fs");
-var indexFile = path.join(__dirname, '../dist/index.html');
-var fileContentOrigin = fs.readFileSync(indexFile, { encoding: 'utf8' });
-var fileContentAfterReplace = fileContentOrigin.replace(/<base href="\/" \/>/g, '<base href="./" />');
-fs.writeFileSync(indexFile, fileContentAfterReplace);
---
+++ electron/electron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $PWD
+mkdir bin || true
+node_modules/.bin/tsc electron/*.ts --outDir bin
+node_modules/.bin/ng build
+node bin/fix-base-tag-for-electron-app.js
+node_modules/.bin/electron bin/main.js
